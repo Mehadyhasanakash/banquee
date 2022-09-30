1b3e6a347718723d205f78282f5f519d109fc5d6
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-09-30 22:56:18 +0600

    hero-complete

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,4 +1,4 @@
-import { CheckCircle, TrendingFlatSharp } from '@mui/icons-material';
+import { CheckCircle, TrendingFlatSharp, Wifi } from '@mui/icons-material';
 import React from 'react';
 import './hero.css'
 
@@ -21,9 +21,9 @@ const Hero = () => {
                        </div>
 
                        <div className='item-seconed'>
-                       <CheckCircle/> <samp className='item-text'>Saving accounts</samp>
+                       <CheckCircle/> <samp className='item-text'>Saving account</samp>
                        <div className='item-right'>
-                       <CheckCircle/> <samp className='item-text'>100% mobile banking</samp>
+                       <CheckCircle className='icon-right'/> <samp className='item-text'>100% mobile banking</samp>
                        </div>
                        </div>
 
@@ -38,7 +38,84 @@ const Hero = () => {
             </div>
 
             <div className="hero-right">
-                <h1>lormkfsdfs</h1>
+                <div className='card-containers'>
+
+                    <div className="card-logo">
+                        <h1>banquee.</h1> <Wifi/>
+                        
+                    
+                    </div>
+
+                    <div className='card-number'>
+                            <h1>1234</h1>
+                            <h1>5678</h1>
+                            <h1>900</h1>
+                            <h1>0000</h1>
+
+                        </div>
+
+                        <div className='card-customer-title'>
+                        <div className="card-title">
+                            <div className='johen-deo'>
+                            <h5>John Doe</h5>
+                            <h3>JCard Holder</h3>
+                            </div>
+
+                            <div className='expir-date'>
+                            <h5>Expiry Date</h5>
+                            <h3>09/28</h3>
+                        </div>
+                        </div>
+
+                        <div>
+                            <h1>VISA</h1>
+                        </div>
+
+                        
+                        </div>
+
+                </div>
+
+
+
+                <div className='card-container'>
+
+                    <div className="card-logo">
+                        <h1>banquee.</h1> <Wifi/>
+                        
+                    
+                    </div>
+
+                    <div className='card-number'>
+                            <h1>1234</h1>
+                            <h1>5678</h1>
+                            <h1>900</h1>
+                            <h1>0000</h1>
+
+                        </div>
+
+                        <div className='card-customer-title'>
+                        <div className="card-title">
+                            <div className='johen-deo'>
+                            <h5>John Doe</h5>
+                            <h3>JCard Holder</h3>
+                            </div>
+
+                            <div className='expir-date'>
+                            <h5>Expiry Date</h5>
+                            <h3>09/28</h3>
+                        </div>
+                        </div>
+
+                        <div>
+                            <h1>VISA</h1>
+                        </div>
+
+                        
+                        </div>
+
+                </div>
+                
                 
             </div>
             </div>
